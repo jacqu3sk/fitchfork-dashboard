59d68949ea7eca6467916dbd964c920a48c67cb1
Jacques Klooster <jacquesk.developer@gmail.com>
2025-06-12 23:35:31 +0200

    fixed command

--- server/routes/run.ts
+++ server/routes/run.ts
@@ -8,7 +8,7 @@ const router = Router();
  * These should be validated and safe, with no user input concatenation.
  */
 const whitelist: Record<string, string> = {
-  "restart-fitchfork": "/bin/systemctl restart fitchfork.service",
+  "restart-fitchfork": "/usr/bin/systemctl restart fitchfork.service",
   "pull-latest": "cd /home/pi/dev/project && /usr/bin/git pull",
   "reboot": "/sbin/reboot",
 };
