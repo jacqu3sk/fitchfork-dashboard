efb73af437b275b780c885cfb5644dae6b614770
Jacques Klooster <jacquesk.developer@gmail.com>
2025-06-12 22:40:58 +0200

    made the refresh toggle better

--- client/src/components/RefreshToggle.tsx
+++ client/src/components/RefreshToggle.tsx
@@ -1,5 +1,5 @@
-import { Button, Switch, Space } from "antd";
 import { ReloadOutlined } from "@ant-design/icons";
+import { Button, Switch, Tooltip } from "antd";
 
 export default function RefreshToggle({
 	onRefresh,
@@ -11,15 +11,20 @@ export default function RefreshToggle({
 	setAutoRefresh: (v: boolean) => void;
 }) {
 	return (
-		<Space.Compact>
-			<Button type="default" icon={<ReloadOutlined />} onClick={onRefresh} />
-			<Switch
-				size="small"
-				checked={autoRefresh}
-				onChange={setAutoRefresh}
-				checkedChildren="Auto"
-				unCheckedChildren="Man"
-			/>
-		</Space.Compact>
+		<div className="flex items-center gap-2 rounded-md border border-gray-200 dark:border-gray-700 px-2 py-1 bg-white dark:bg-gray-900">
+			<Tooltip title="Refresh now">
+				<Button
+					type="text"
+					icon={<ReloadOutlined />}
+					onClick={onRefresh}
+					className="!text-gray-700 dark:!text-gray-200 !p-1"
+				/>
+			</Tooltip>
+
+			<div className="flex items-center gap-1">
+				<span className="text-xs text-gray-500 dark:text-gray-400">Auto</span>
+				<Switch size="small" checked={autoRefresh} onChange={setAutoRefresh} />
+			</div>
+		</div>
 	);
 }
